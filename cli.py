749d3772caf3df3c4c8c882c63c57ffcf2c13fe6
Bigh <76061149+HenriGuillaume@users.noreply.github.com>
2025-06-02 16:26:38 +0200

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,108 @@
+import argparse
+import models
+import processing
+import conv
+import sys, os
+import torch
+from typing import List, Tuple
+
+
+nasal_merge_map = {
+    'a': 'ã',
+    'e': 'ẽ',
+    'o': 'õ',
+    '9': 'œ̃',
+}
+
+def merge_nasal_phonemes(
+        segments: List[Tuple[float, float, str]]
+        ) -> List[Tuple[float, float, str]]:
+        """
+        Merge sequences like ('a', '~') into nasalized phonemes like 'ã'.
+        """
+        merged = []
+        i = 0
+        while i < len(segments):
+            start, end, label = segments[i]
+            label = label.strip()
+            
+            # Check if next segment is a nasal diacritic
+            if i + 1 < len(segments):
+                next_start, next_end, next_label = segments[i + 1]
+                next_label = next_label.strip()
+                
+                if next_label == '~' and label in nasal_merge_map:
+                    nasal = nasal_merge_map[label]
+                    merged.append((start, next_end, nasal))
+                    i += 2
+                    continue
+            
+            # If no merge happened
+            merged.append([start, end, label])
+            i += 1 
+        return merged
+
+
+def silence_long_phonemes(
+        segments: List[Tuple[float, float, str]],
+        length_thresh = 1.5
+        ):
+    '''Mark phonemes longer than a threshold as silent'''
+    for i, (start, end, phon) in enumerate(segments):
+        if abs(start - end) > length_thresh:
+            segments[i] = (start, end, '~')
+    return segments
+
+
+def merge_repeated_phonemes(
+        segments: List[Tuple[float, float, str]],
+        ):
+    '''Merge adjacent phonemes that are the same'''
+    i = 0
+    while i < len(segments) - 1:
+        start1, end1, phon1 = segments[i]
+        start2, end2, phon2 = segments[i + 1]
+        if phon1 == phon2:
+            segments[i] = (start1, end2, phon1)
+            del segments[i + 1]
+        else:
+            i += 1
+    return segments
+
+
+def ctc_kreuk_pipeline(
+        audio_pth: str,
+        ckpt_pth: str,
+        voice_detection = True
+        ):
+    audio_data = processing.load_audio(audio_pth)
+    whole_speech_seg = [(0, len(audio_data.signal) / audio_data.fs, 1)]
+    if voice_detection:
+        active_seg = models.global_VAD(audio_pth)
+    else:
+        active_seg = whole_speech_seg
+    # inference
+    ctc_seg = models.global_ctc_phoneme_alignment(audio_data.signal, audio_data.fs, whole_speech_seg)
+    # run phoneme boundary detection on active segments
+    kreuk_seg = models.global_kreuk_seg(audio_data.signal, audio_data.fs, active_seg, ckpt_pth)
+    # adjusting boundaries
+    phon_seg = models.ctc_kreuk_heuristic_match(ctc_seg, kreuk_seg)
+    # tweaks
+    phon_seg = silence_long_phonemes(phon_seg)
+    phon_seg = merge_repeated_phonemes(phon_seg)
+    phon_seg = merge_nasal_phonemes(phon_seg)
+    # save
+    base_name = os.path.splitext(os.path.basename(audio_pth))[0]
+    output_path = os.path.join("outputs", f"{base_name}_seg.TextGrid")
+    conv.seg_to_textgrid(phon_seg, output_path)
+    return phon_seg
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run CTC-Kreuk phoneme alignment pipeline.")
+    parser.add_argument("--audio", required=True, help="Path to input audio file.")
+    parser.add_argument("--ckpt", required=True, help="Path to Kreuk model checkpoint.")
+    args = parser.parse_args()
+
+    ctc_kreuk_pipeline(args.audio, args.ckpt)
